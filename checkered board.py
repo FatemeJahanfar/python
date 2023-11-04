4a3bce22a62d0f9aa9a3f66c3829b0a01cddedb4
FatemeJahanfar <148343211+FatemeJahanfar@users.noreply.github.com>
2023-11-04 23:28:13 +0330

    Add files via upload

--- checkered board.py
+++ checkered board.py
@@ -1,10 +1,9 @@
-m=int(input("what's the row of the board? "))
-n=int(input("what's the column of the board? "))
-a=[]
-for i in range(m):
-    row=["*#"]*n
-    a+=[row]
-for i in range(m):
-    for j in range(n):
-        print(a[i][j],end=" ")
+n=int(input("what's the row of the board? :"))
+m=int(input("what's the column of the board? :"))
+for i in range(n):
+    for j in range(m):
+        if(i+j)%2==0:
+            print("#",end="")
+        else:
+            print("*",end=" ")
     print()
